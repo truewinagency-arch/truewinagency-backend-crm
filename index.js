7b936670120f645ec282ceeb9a50af2bc901ba0e
truewinagency-arch <truewinagency@gmail.com>
2026-07-14 11:04:28 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -555,7 +555,7 @@ app.post('/send-text', async (req, res) => {
 
                         // 2. CREAMOS LA MINIATURA DE CARGA RÁPIDA (Sin deformar, anti-pixelado)
                         const thumbImage = image.clone();
-                        thumbImage.scaleToFit(300, 300).quality(80); 
+                        thumbImage.scaleToFit(100, 100).quality(80); 
                         let bufferProcesado = await thumbImage.getBufferAsync(Jimp.MIME_JPEG);
                         
                         if (bufferProcesado.length < 64000) {
